d3d844dd2fd3826e71d6b773f6f7fd473bc5bb0b
Chris Baum <christopher.baum@valpo.edu>
2021-02-17 02:06:19 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -24,7 +24,6 @@ class MusicLibraryController
   def list_songs
     count = 1
     sortedlist = Song.all.sort{|a, b| a.name <=> b.name}
-    binding.pry
     Song.all.sorteach do |song|
       puts "#{count}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
     end
