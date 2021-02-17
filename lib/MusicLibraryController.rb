3fb284fd4f586bac4efb76da0d4396dab010b51e
Chris Baum <christopher.baum@valpo.edu>
2021-02-17 02:34:21 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -78,6 +78,7 @@ class MusicLibraryController
     puts "Which song number would you like to play?"
     input = gets.strip.to_i
     @songssortedlist
+    binding.pry
     #puts "Playing #{song.name} by #{song.artist.name}"
   end
 end
